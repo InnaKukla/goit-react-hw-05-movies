e733d8154a425c97055a6117ec39d13c6ab01893
Inna Kukla <0969125133@ukr.net>
2023-03-16 01:18:33 +0200

    Create Reviews.styled.js

---
+++ src/components/Reviews/Reviews.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ReviewsWrap = styled.ul`
+  list-style-type: disclosure-closed;
+`;
+
+export const ReviewsAuthor = styled.p`
+  margin-left: 20px;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+  font-size: 18px;
+  padding: 3px 10px;
+  border: 0.5px solid lightgray;
+`;
+
+export const ReviewsAuthorName = styled.span`
+  font-family: 'Comic Sans MS';
+  font-size: 20px;
+  font-weight: 600;
+`;
+export const ReviewsContext = styled.p`
+  line-height: 1.6;
+`;
